3ffb26fac8617e00473a2030a7d9d9ab0571b828
Ivan Redreev <redreev.ivan@gmail.com>
2021-02-15 20:35:45 +0500

    Solution

--- CalendarApp/ViewController.swift
+++ CalendarApp/ViewController.swift
@@ -6,14 +6,49 @@
 //
 
 import UIKit
+import EventKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet var dateLabel: UILabel!
+    
+    var startDate = Date()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        let dataText = dateLabel.text ?? ""
+        let dateFormatter = DateFormatter()
+        dateFormatter.locale = Locale(identifier: "en_US_POSIX")
+        dateFormatter.dateFormat = "dd-MM-yyyy HH:mm"
+        dateFormatter.timeZone = TimeZone(secondsFromGMT: 18000)
+        if let date = dateFormatter.date(from: dataText) {
+            startDate = date
+        }
     }
-
-
+    
+    @IBAction func addEvent() {
+        let store = EKEventStore()
+        store.requestAccess(to: .event) { (granted, error) in
+            if let error = error {
+                print("request access error: \(error)")
+            } else if granted {
+                print("access granted")
+            } else {
+                print("access denied")
+            }
+        }
+        
+        let event = EKEvent(eventStore: store)
+        let endDate = Date(timeInterval: 1800, since: startDate)
+        event.calendar = store.defaultCalendarForNewEvents
+        event.title = "Запись на сервис"
+        event.startDate = startDate
+        event.endDate = endDate
+        do {
+            try store.save(event, span: .thisEvent)
+        } catch {
+            print("saving event error: \(error)")
+        }
+    }
+    
 }
 
